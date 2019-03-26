0a4d62b075bfea70db211dc7b196bfdd24c80481
Ramon Ribeiro <rhpr@cesar.org.br>
2019-03-26 14:18:34 -0300

    Clone cloud repositories in cli
    Change-Id: I3f9fab9790ecc337a49906a059a6303f3bb4083b

--- cli/index.js
+++ cli/index.js
@@ -1,16 +1,31 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 import yargs from 'yargs';
+import gitClone from 'git-clone';
+
+const REPOSITORIES = [
+  'CESARBR/knot-cloud-ui',
+  'CESARBR/knot-cloud-protocol-adapter-websocket',
+  'CESARBR/knot-cloud-authenticator',
+  'CESARBR/knot-cloud-storage',
+];
 
 yargs // eslint-disable-line no-unused-expressions
   .command('init [path]', 'init stack', (_yargs) => {
     _yargs
       .positional('path', {
         describe: 'path destination to clone repositories',
-        default: `${process.env.HOME}/cloud`,
+        default: process.env.HOME,
       });
-  }, () => {
-    console.log('Starting...');
+  }, (args) => {
+    REPOSITORIES.forEach((repo) => {
+      console.log(`Trying to clone repository: ${repo}`);
+      gitClone(
+        `http://github.com/${repo}`,
+        `${args.path}/cloud/${repo.split('/')[1]}`,
+        err => (err ? console.error(err) : console.log('Cloned!')),
+      );
+    });
   })
   .demandCommand()
   .help()
