f4a721ee33ec756f73a8c181cdab349f88140af4
Ramon Ribeiro <rhpr@cesar.org.br>
2019-03-26 14:18:34 -0300

    Clone cloud repositories in cli
    Change-Id: I3f9fab9790ecc337a49906a059a6303f3bb4083b

--- src/index.js
+++ src/index.js
@@ -1,16 +1,56 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
 import yargs from 'yargs';
+import gitClone from 'git-clone';
+import fs from 'fs';
+import path from 'path';
+
+const REPOSITORIES = [
+  'CESARBR/knot-cloud-ui',
+  'CESARBR/knot-cloud-protocol-adapter-websocket',
+  'CESARBR/knot-cloud-authenticator',
+  'CESARBR/knot-cloud-storage',
+  'CESARBR/knot-cloud-bootstrap',
+];
+
+const logFileMessageRepository = `
+        This is mostly caused when the repository is already installed
+        on the destination directory or if there is a connection problem.
+        Check ${path.resolve('knot.err')} for more details about the issue\n`;
+
+function handleError(err, msg, logFileMessage) {
+  console.error(msg);
+  console.error(logFileMessage);
+  fs.writeFileSync('knot.err', `${msg}\n`, { flag: 'a', encoding: 'utf-8' });
+  fs.writeFileSync('knot.err', `${err.stack}\n`, { flag: 'a', encoding: 'utf-8' });
+}
 
 yargs // eslint-disable-line no-unused-expressions
   .command('init [path]', 'initialize stack', (_yargs) => {
     _yargs
       .positional('path', {
         describe: 'path where to clone repositories',
-        default: `.`,
+        default: '.',
       });
-  }, () => {
-    console.log('Starting...');
+  }, (args) => {
+    fs.unlinkSync('knot.err');
+    REPOSITORIES.forEach((repo) => {
+      const repoName = repo.split('/')[1];
+      const repoPath = `${args.path}/${repoName}`;
+      console.log(`Cloning: ${repoName}`);
+      gitClone(
+        `http://github.com/${repo}`,
+        repoPath,
+        (err) => {
+          if (err) {
+            const msg = `[Error]:\n\tAn error occured while cloning repository ${repoName}`;
+            handleError(err, msg, logFileMessageRepository);
+            return;
+          }
+          console.log(`Cloned: ${repoName}`);
+        },
+      );
+    });
   })
   .demandCommand()
   .help()
