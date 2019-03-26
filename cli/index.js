7e7ed96fb02693366beb2f1eac193bbafe2166a5
Ramon Ribeiro <rhpr@cesar.org.br>
2019-03-26 13:50:04 -0300

    Add init command to cli
    Change-Id: I6b8a1fdbab54fc0ad4e335f514d7c15b5a279088

---
+++ cli/index.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+import yargs from 'yargs';
+
+yargs // eslint-disable-line no-unused-expressions
+  .command('init [path]', 'init stack', (_yargs) => {
+    _yargs
+      .positional('path', {
+        describe: 'path destination to clone repositories',
+        default: `${process.env.HOME}/cloud`,
+      });
+  }, () => {
+    console.log('Starting...');
+  })
+  .demandCommand()
+  .help()
+  .strict()
+  .argv;
