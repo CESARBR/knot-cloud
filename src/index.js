386089a8cd77f4623d1c1f6f1c5028bbae2451e3
Paulo Serra Filho <ptvsf@cesar.org.br>
2019-03-28 10:36:21 -0300

    Copy development stack to current directory
    Change-Id: I68c4d7c846cb71deaada1b604b395564751aec05

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 /* eslint-disable no-console */
 import yargs from 'yargs';
 import gitClone from 'git-clone';
+import fs from 'fs-extra';
+import path from 'path';
 
 const REPOSITORIES = [
   'CESARBR/knot-cloud-ui',
@@ -10,6 +12,27 @@ const REPOSITORIES = [
   'CESARBR/knot-cloud-storage',
   'CESARBR/knot-cloud-bootstrap',
 ];
+
+const cpDevDir = (basePath) => {
+  const stackDir = path.join(basePath, 'stack');
+  try {
+    if (fs.existsSync(stackDir)) {
+      fs.removeSync(stackDir);
+    }
+    fs.ensureDirSync(stackDir);
+    fs.copySync('stacks/dev', stackDir);
+  } catch (err) {
+    console.error(`[Error]:
+    An error occurred while copying the development stack files.
+    This is mostly associated with write permissions on the destination directory,
+    check knot.err for details on the issue.`);
+    fs.writeJsonSync('knot.err',
+      {
+        Description: 'Error copying development stack',
+        Error: err,
+      });
+  }
+};
 
 yargs // eslint-disable-line no-unused-expressions
   .command('init [path]', 'initialize stack', (_yargs) => {
@@ -27,6 +50,7 @@ yargs // eslint-disable-line no-unused-expressions
         err => (err ? console.error(err) : console.log('Cloned!')),
       );
     });
+    cpDevDir(args.path);
   })
   .demandCommand()
   .help()
